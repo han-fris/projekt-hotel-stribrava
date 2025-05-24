bd3389e1d3ac213234f5db30e02765542a7047bd
Han Frisonsova <hanafrisonsova@gmail.com>
2025-05-24 14:34:52 +0200

    stahne api do HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -2,8 +2,23 @@ import { Header } from '../../components/Header/Header';
 import { ListRoom } from '../../components/ListRoom/ListRoom';
 import { RoomDetail } from '../../components/RoomDetail/RoomDetail';
 import { Footer } from '../../components/Footer/Footer';
+import { useState, useEffect } from 'react';
 
 export const HomePage = () => {
+  const [rooms, setRooms] = useState([]);
+  const [selectedRoom, setSelectedRoom] = '';
+
+  useEffect(() => {
+    const fetchedRooms = async () => {
+      const response = await fetch('http://localhost:4000/api/rooms');
+      const responseData = await response.json();
+      setRooms([responseData.data]);
+    };
+    fetchedRooms();
+  }, []);
+
+  console.log(rooms);
+
   return (
     <>
       <Header />
